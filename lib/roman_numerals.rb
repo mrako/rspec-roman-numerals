aefb0cfab5b43ad673624e09b7da01b10299c9fb
Marko Klemetti <marko.klemetti@gmail.com>
2015-03-29 23:39:47 +0300

    initial commit

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,19 @@
+class RomanNumerals
+  ROMAN_MAPPING = {
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
+
+  def self.convert(number)
+    roman = ""
+    ROMAN_MAPPING.keys.each do |divisor|
+      quotient, modulus = number.divmod(divisor)
+      roman << ROMAN_MAPPING[divisor] * quotient
+      number = modulus
+    end
+    roman
+  end
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,21 @@
+require "roman_numerals"
+
+describe "Roman Numerals Converter" do
+  subject{ RomanNumerals }
+
+  it "should return 'I' for 1" do
+    expect(subject.convert(1)).to eq('I')
+  end
+  
+  it "should return 'II' for 2" do
+    expect(subject.convert(2)).to eq('II')
+  end
+  
+  it "should return 'V' for 5" do
+    expect(subject.convert(5)).to eq('V')
+  end
+  
+  it "should return 'X' for 10" do
+    expect(subject.convert(10)).to eq('X')
+  end
+end
